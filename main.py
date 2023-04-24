47e3867f87b951e003d1be91b00bbd19170b024f
DanTimoshenko <123253263g@gmail.com>
2023-04-24 22:15:20 +0300

    add file

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-print("hi pyton")
-x = 2
-y = 3
-z = x + y
-print(z)
-grettings = "hello pyton"
-print(grettings)
-print("my age is", z)
-adadwa
+print('Мультипарадигмальні мови програмування', 'лабораторна номер', 1)
