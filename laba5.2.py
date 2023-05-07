1033b7660ad8f93be3584088b241a01f72553fab
DanTimoshenko <113703529+DanTimoshenko@users.noreply.github.com>
2023-05-07 23:56:42 +0300

    Update laba5.2.py

--- laba5.2.py
+++ laba5.2.py
@@ -1,6 +1,15 @@
-n = int(input('Введіть ціле число: '))
-count = 0
-while n > 0:
-    count += 1
-    n //= 10
-print('Кількість цифр у числі:', count)
+num = int(input('Enter an integer'))
+if num == 0:
+    print('The number of digits is 1')
+elif num > 0:
+    count = 0
+    while num:
+        count += 1
+        num //= 10
+    print('The number of digits in a number', count)
+else:
+    count = -1
+    while num:
+        count += 1
+        num //= -10
+    print('The number of digits in a number', count)
