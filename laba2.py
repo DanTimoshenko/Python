538848a44d2fe72d5c677feec505ae621734acc4
DanTimoshenko <123253263g@gmail.com>
2023-05-31 01:05:22 +0300

    labatest1

--- laba2.py
+++ laba2.py
@@ -3,7 +3,7 @@
 print('''IKM221K lab number 2
 Timoshenko Daniil 18''')
 
-TEMPLATE = 'input please { }: '
+TEMPLATE = 'input please {}: '
 x = int(input(TEMPLATE.format('x')))
 y = int(input(TEMPLATE.format('y')))
 z = int(input(TEMPLATE.format('z')))
