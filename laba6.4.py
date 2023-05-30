744c8d38616b38d4184444f597752ca5095ca153
DanTimoshenko <113703529+DanTimoshenko@users.noreply.github.com>
2023-05-31 01:57:51 +0300

    Delete laba6.4.py

--- laba6.4.py
+++
@@ -1,7 +0,0 @@
-filename = 'learning_python.txt'
-
-with open(filename, 'r') as file:
-    for line in file:
-        modified_line = line.replace('Python ', 'C ')
-        print(modified_line.strip())
-
